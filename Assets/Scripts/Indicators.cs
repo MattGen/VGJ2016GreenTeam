827b8f74e41122b0d2b4f0721eb3377da5c9b9c5
Ludovic Feltz <ludovic.feltz@softfluent.com>
2016-11-05 07:29:58 +0100

    Indicators

---
+++ Assets/Scripts/Indicators.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Indicators : MonoBehaviour
+{
+    public InterractableObject Valve;
+    public Image ValveIndicator;
+
+	
+	// Update is called once per frame
+	void Update ()
+	{
+        var ratio = Valve.RotationTime / Valve.currentRotationTime;
+	    ValveIndicator.fillAmount = ratio;
+        ValveIndicator.color = new Color(1-ratio, ratio, 0);
+	}
+}
--- Assets/Scripts/InterractableObject.cs
+++ Assets/Scripts/InterractableObject.cs
@@ -4,7 +4,8 @@ using System.Collections;
 public class InterractableObject : MonoBehaviour {
 
 	public float RotationTime;
-	private float currentRotationTime;
+    [HideInInspector]
+	public float currentRotationTime;
 	private float timeSinceLastRotation;
 
 	public void Start() {
